7c9bd2c4f1abc31659d54e0422ad9c0fecf4a734
Martina <martapetkovska@gmail.com>
2026-03-02 11:37:11 +0100

    Update Database Schema

---
+++ Planity/Migrations/202602241457331_UpdateDatabaseSchema.cs
@@ -0,0 +1,18 @@
+﻿namespace Planity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabaseSchema : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.TaskItems", "AttachedFilePath");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.TaskItems", "AttachedFilePath", c => c.String());
+        }
+    }
+}
---
+++ Planity/Migrations/202603021035043_UpdateDatabaseSchema1.cs
@@ -0,0 +1,18 @@
+﻿namespace Planity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabaseSchema1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.TaskItems", "AttachedFilePath", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.TaskItems", "AttachedFilePath");
+        }
+    }
+}
